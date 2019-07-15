ae158f6c0d3530fd3553faf4a0c8f1c6c3966dcf
dimoka <dimokavip777@gmail.com>
2019-07-15 22:28:44 +0600

    todo

---
+++ do_list/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=40,
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control', 'placeholder': 'Add Your todo',
+                                      'aria-label': 'Todo', 'aria-describedby': 'button-addon2'}))
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dimoka/Django/to_do_list/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
